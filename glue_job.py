a2b68aa6e362849a8d00406fcd211374eaf7ad47
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2024-05-24 13:35:12 -0400

    Create glue_job.py

---
+++ glue_job.py
@@ -0,0 +1,36 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+# Set up Glue context
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Read data from S3
+source_df = glueContext.create_dynamic_frame.from_catalog(
+    database="your_database",
+    table_name="your_table"
+)
+
+# Perform transformations
+# Example transformation: Convert data types, filter rows, etc.
+transformed_df = source_df.apply_mapping([
+    ("source_column", "string", "target_column", "int"),
+    # Add more transformations as needed
+])
+
+# Write data to Redshift
+glueContext.write_dynamic_frame.from_jdbc_conf(
+    frame=transformed_df,
+    catalog_connection="redshift_connection",
+    connection_options={"dbtable": "your_redshift_table", "database": "your_redshift_db"},
+    redshift_tmp_dir="s3://your-temporary-s3-bucket/temp"
+)
+
+job.commit()
